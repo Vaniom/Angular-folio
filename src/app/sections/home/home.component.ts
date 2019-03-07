8b1d0663fe3ce29a129e4a4dd8a00bd44d64e9b5
Florent Pianet <florent.p83@gmail.com>
2019-03-07 19:21:34 +0100

    Mise en page de la section accueil

--- src/app/sections/home/home.component.ts
+++ src/app/sections/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,8 +9,11 @@ import { Component } from '@angular/core';
 
 export class HomeComponent {
 
-  something: any;
+  constructor(private router: Router) {
 
-  constructor() {
-   }
+  }
+
+  onGoToContact() {
+    this.router.navigate(['/contact']);
+  }
 }
