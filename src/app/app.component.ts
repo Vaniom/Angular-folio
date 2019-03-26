d5294e3c4699d437c5fb757b99a8b5d395baa4e3
Florent Pianet <florent.p83@gmail.com>
2019-03-26 19:31:34 +0100

    internationalisation module i18n.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, LOCALE_ID, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'foliogular';
+
+  languageList = [
+    { code: 'fr', label: 'Français' },
+    { code: 'en', label: 'English' }
+  ];
+
+  constructor(@Inject(LOCALE_ID) protected localeId: string) { }
+
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  @Input() languageList: any[];
+
   isNavbarCollapsed = true;
 }
